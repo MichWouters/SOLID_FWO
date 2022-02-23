61ccbd47168f5823f268d957de7fe6402594230c
Michiel Wouters <michiel_woute64@hotmail.com>
2022-02-23 12:57:24 +0100

    Implemented OCP

---
+++ ArdalisRating/Raters/LandPolicyRater.cs
@@ -0,0 +1,28 @@
+﻿namespace ArdalisRating.Raters
+{
+    public class LandPolicyRater : Rater
+    {
+        public LandPolicyRater(Policy policy) : base(policy)
+        {
+        }
+
+        public override void Rate(RatingEngine engine)
+        {
+            Logger.Log("Rating LAND policy...");
+            Logger.Log("Validating policy.");
+            if (Policy.BondAmount == 0 || Policy.Valuation == 0)
+            {
+                Logger.Log("Land policy must specify Bond Amount and Valuation.");
+                return;
+            }
+
+            if (Policy.BondAmount < 0.8m * Policy.Valuation)
+            {
+                Logger.Log("Insufficient bond amount.");
+                return;
+            }
+
+            engine.Rating = Policy.BondAmount * 0.05m;
+        }
+    }
+}
